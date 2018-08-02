11734d099d8912244cab32113edd3ec8e8aa8517
Szymon Bluma <szymon.bluma@polcode.net>
2018-08-02 15:49:32 +0200

    whereNotIn criteria

---
+++ src/Criteria/WithAttributeNotInValuesCriterion.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Napp\Core\Dbal\Criteria;
+
+class WithAttributeNotInValuesCriterion implements CriterionInterface
+{
+    /**
+     * @var string
+     */
+    protected $attribute;
+
+    /**
+     * @var array
+     */
+    protected $values;
+
+    /**
+     * @param string $attribute
+     * @param array $values
+     */
+    public function __construct($attribute, $values)
+    {
+        $this->attribute = $attribute;
+        $this->values = $values;
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply($query)
+    {
+        $query->getQuery()->whereNotIn($query->getModel()->getTable() . '.' . $this->attribute, $this->values);
+
+        return $query;
+    }
+}
